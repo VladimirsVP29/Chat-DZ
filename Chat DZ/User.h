852b8c111440fa9076a6ddacd864fa168b591efd
VladimirsVP29 <vnorgep@outlook.com>
2024-01-21 11:54:33 +0100

    #Class User , login,password,name

---
+++ Chat DZ/User.h
@@ -0,0 +1,26 @@
+#pragma once
+#include<string>
+
+class User
+{
+public:
+	User(const std::string& login, const std::string& password, const std::string& name)
+		: login_(login), password_(password), name_(name)
+	{
+
+	}
+	const std::string& getUserLogin() const { return login_; }
+
+	const std::string& getUserPassword() const { return password_; }
+	void setUserPassword(const std::string& password) { password_ = password; }
+
+	const std::string& getUserName() const { return name_; }
+	void setUserName(const std::string& name) { name_ = name; }
+
+private:
+	const std::string login_;
+	std::string password_;
+	std::string name_;
+};
+
+
