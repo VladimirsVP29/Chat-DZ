232133895810dbd41d1934e904b0b0bb91e84d96
VladimirsVP29 <vnorgep@outlook.com>
2024-01-21 14:22:37 +0100

    #getUserByLogin

--- Chat DZ/Chat.cpp
+++ Chat DZ/Chat.cpp
@@ -5,3 +5,15 @@ void Chat::start()
 {
 	isChatWork_ = true;
 }
+
+std::shared_ptr<User> Chat::getUserByLogin(const std::string& login) const
+{
+	for (auto &user : users_)
+	{
+		if (login == user.getUserLogin())
+		{
+			return std::make_shared<User>(user);
+		}
+	}
+	return nullptr;
+}
