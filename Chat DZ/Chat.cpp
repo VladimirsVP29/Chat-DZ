0444df3d49d0e525f29c069929f550fa88c6dddf
VladimirsVP29 <vnorgep@outlook.com>
2024-01-21 14:50:08 +0100

    #login

--- Chat DZ/Chat.cpp
+++ Chat DZ/Chat.cpp
@@ -6,6 +6,34 @@ void Chat::start()
 	isChatWork_ = true;
 }
 
+void Chat::login()
+{
+	std::string login, password;
+	char operation;
+
+	do
+	{
+		std::cout << "login: ";
+		std::cin >> login;
+		std::cout << "password: ";
+		std::cin >> password;
+
+		currentUser_ = getUserByLogin(login);
+
+		if (currentUser_ == nullptr || (password != currentUser_->getUserPassword()))
+		{
+			currentUser_ = nullptr;
+
+			std::cout << "login failed..." << std::endl;
+			std::cout << "(0)exit or (any key)repeat: ";
+			std::cin >> operation;
+
+			if (operation == '0')
+				break;
+		}
+	} while (!currentUser_);
+}
+
 std::shared_ptr<User> Chat::getUserByLogin(const std::string& login) const
 {
 	for (auto &user : users_)
@@ -29,3 +57,4 @@ std::shared_ptr<User> Chat::getUserByName(const std::string& name) const
 	}
 	return nullptr;
 }
+
